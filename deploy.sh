80770119a2b0d5f4bdc6dfb7cb973f20def4db4f
FaintGhost <zhang.yaowei@live.com>
2020-05-26 18:03:43 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,15 @@ checkRoot(){
     echo "执行脚本!"
 }
 
+install_docker(){
+    sudo apt-get update -y
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+}
+
 deploy_portainer(){
     echo -e "开始部署Portainer"
     cd $DEPLOY_PATH/portainer
