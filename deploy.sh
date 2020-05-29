4679aa14b93327cc3e08f0d580a7fb4a2eb82361
FaintGhost <zhang.yaowei@live.com>
2020-05-29 17:41:54 +0200

    bug fixed

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ install_docker(){
     sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+    sudo apt-get update -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 }
 
 deploy_portainer(){
