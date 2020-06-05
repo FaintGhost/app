1a03fb4639758fc61775395cc6940d3a3ae51312
FaintGhost <zhang.yaowei@live.com>
2020-06-06 00:34:17 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -28,54 +28,48 @@ install_docker() {
 
 deploy_portainer() {
     echo -e "开始部署Portainer"
-    cd $DEPLOY_PATH/portainer
-    docker-compose up -d
+    docker-compose -f $DEPLOY_PATH/portainer/docker-compose.yml up -d
 }
 
 deploy_nginx() {
     echo -e "开始部署Nginx Proxy Manager"
-    cd $DEPLOY_PATH/nginx
-    docker-compose up -d
+    docker-compose -f $DEPLOY_PATH/nginx/docker-compose.yml up -d
 }
 
 deploy_adguardhome() {
     echo -e "开始部署AdGuardHome"
-    cd $DEPLOY_PATH/adguardhome
-    docker-compose up -d
+    docker-compose -f $DEPLOY_PATH/adguardhome/docker-compose.yml up -d
 }
 
 deploy_nodered() {
     echo -e "开始部署NodeRED"
-    sudo chown -R 1000:1000 $DEPLOY_PATH/nodered
-    cd $DEPLOY_PATH/nodered
-    docker-compose up -d
+    sudo chown -R 1000:1000 $DEPLOY_PATH/nodered/data
+    rm $DEPLOY_PATH/nodered/data/.gitkeep
+    docker-compose -f $DEPLOY_PATH/nodered/docker-compose.yml up -d
 }
 
 deploy_ttrss() {
     echo -e "开始部署Tiny Tiny RSS"
-    cd $DEPLOY_PATH/ttrss
-    docker-compose up -d
-}
-
-install_ha() {
-    cd ~
-    apt-get install software-properties-common
-    apt-get update -y
-    apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
-    curl -sL https://raw.githubusercontent.com/FaintGhost/supervised-installer/master/installer.sh | bash -s -- -m intel-nuc
+    docker-compose -f $DEPLOY_PATH/ttrss/docker-compose.yml up -d
 }
 
 deploy_watchtower() {
     echo -e "开始部署Watchtower"
-    cd $DEPLOY_PATH/watchtower
-    docker-compose up -d
+    docker-compose -f $DEPLOY_PATH/watchtower/docker-compose.yml up -d
+}
+
+install_ha() {
+    cd ~
+    apt-get install software-properties-common -y
+    apt-get update -y
+    apt-get install apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat -y
+    curl -sL https://raw.githubusercontent.com/FaintGhost/supervised-installer/master/installer.sh | bash -s -- -m intel-nuc
 }
 
 deploy() {
     echo "----------------------------------------"
     checkRoot
     deploy_adguardhome
-    echo "----------------------------------------"
     echo "----------------------------------------"
     deploy_portainer
     echo "----------------------------------------"
